76dda3759256f82bb5a30186c65a471e3d63002f
043_Janki_Kumari <145994047+043-Janki@users.noreply.github.com>
2025-03-05 14:58:50 +0000

    Added React APp to the repository

---
+++ REACT/01basicreact/src/App.js
@@ -0,0 +1,18 @@
+// function App() {
+//   return (
+//     <h1>chai aur react | Janki</h1>
+//   );
+// }
+
+import Chai from './Chai'
+
+function App(){
+  return(
+    <>
+    <Chai />
+    <h1>chai aur react | </h1>
+    </>
+  )
+}
+
+export default App;
---
+++ REACT/vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+    // // <h1>Chai aur react with Vite | Janki</h1>
+    
+    // <Chai/>
+    // // we can export only one element
+    // // or
+
+    <>
+    <Chai/>
+    <h1>Chai aur React</h1>
+    <p>test para</p>
+    </>
+  )
+}
+
+export default App
